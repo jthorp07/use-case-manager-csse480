16beb8d480631a64be45b23a6b23ca46f3940de9
Jack Thorp <jackthorp07@gmail.com>
2023-10-22 14:46:28 -0400

    added a private sort to use case class

--- use_case_manager/lib/model/use_case.dart
+++ use_case_manager/lib/model/use_case.dart
@@ -4,16 +4,24 @@ import 'package:use_case_manager/model/use_case_flow.dart';
 class UseCase {
   final List<UseCaseFlow> _flows = List.empty(growable: true);
   int _currentFlow = 0;
-
   String title;
-
   List<Actor> actors;
 
   UseCase({required this.title, required this.actors}) {
     _flows.add(UseCaseFlow(title: "Basic", type: FlowType.basic));
   }
 
-  void addFlow() {}
+  // ************************************
+  //
+  // Public Methods
+  //
+  // ************************************
+
+  void addFlow({required String flowName, required FlowType type}) {
+
+    _flows.add(UseCaseFlow(title: flowName, type: type));
+    _sortFlows();
+  }
 
   void nextFlow() {
     if (_currentFlow == _flows.length) return;
@@ -31,5 +39,14 @@ class UseCase {
 
   void prevStep() {
     _flows[_currentFlow].nextStep();
+  }
+
+  // ************************************
+  //
+  // Private Methods
+  //
+  // ************************************
+  void _sortFlows() {
+    _flows.sort((a, b) => a.compareTo(b));
   }
 }
--- use_case_manager/lib/model/use_case_flow.dart
+++ use_case_manager/lib/model/use_case_flow.dart
@@ -1,6 +1,6 @@
 enum FlowType { basic, alternate, exception }
 
-class UseCaseFlow {
+class UseCaseFlow implements Comparable<UseCaseFlow> {
   final String title;
   final List<String> steps = List.empty(growable: true);
   int currentStep = 0;
@@ -21,5 +21,21 @@ class UseCaseFlow {
   void nextStep() {
     if (currentStep == steps.length) return;
     currentStep++;
+  }
+
+  @override
+  int compareTo(UseCaseFlow other) {
+    switch (type) {
+      case FlowType.basic:
+        return other.type == FlowType.basic ? 0 : -1;
+      case FlowType.alternate:
+        return other.type == FlowType.basic
+            ? 1
+            : other.type == FlowType.alternate
+                ? 0
+                : -1;
+      case FlowType.exception:
+        return other.type == FlowType.exception ? 0 : 1;
+    }
   }
 }
